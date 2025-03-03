bef8c91c2a9f994524b8e025ee55858c82027295
s-A301-15 <s-A301-15@K-A301-15>
2025-03-03 19:40:23 +0100

    Dodaj pliki projektów.

---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace EShopService.Models
+{
+    public class Product : BaseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public string ean { get; set; } = default!;
+
+        public decimal price {get; set; }
+
+        public int stock { get; set; }
+
+        public string sku { get; set; } = default!;
+
+        public Category category { get; set; } = default!;
+
+    }
+}
